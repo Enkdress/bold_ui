01a5e8b44f26c0de2501407036fb529ce37e7dd7
Sergio Correa <felizjanuk12@gmail.com>
2021-05-11 14:59:52 -0500

    Setting up the project

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import "antd/dist/antd.css";
+import "assets/fonts.scss";
+import "assets/variables.scss";
+
+import Layout from "components/layout";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
